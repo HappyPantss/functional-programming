908de7ef9d3c9961f3fc77b9c52c8d38547f5256
HappyPantss <sergioeijben@hotmail.com>
2020-10-23 15:05:09 +0200

    fixed fancy colors

--- static/js/script.js
+++ static/js/script.js
@@ -9,16 +9,16 @@ const hexGreen = "009900"
 const rgbCode = "(139,69,19)"
 
 // Source: https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-function componentToHex (c) {
-    let hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+let componentToHex = (c) => {
+    let hex = c.toString(16); // Kijk naar de cijfers die worden meegegeven, en deel deze per keer door 16 
+    return hex.length == 1 ? "0" + hex : hex; // Als hex.length is 1 zet een "0" neer.. anders is het de value van let hex (hex / 16)
 }
 
-function rgbToHex(r, g, b) {
-    return componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
+let rgbToHex = (r, g, b) => {
+    return componentToHex(r) + componentToHex(g) + componentToHex(b); // Zet alle gedeelde cijfers achter elkaar.. dit maakt de hexcode
+} 
 
-// Arrow function
+// Arrow function eye colors
 const replaceAll = surveyAnswers.map(answer => answer[colomnName]
     .replace("#", "")
     .replace(" ", "")
@@ -31,32 +31,36 @@ const replaceAll = surveyAnswers.map(answer => answer[colomnName]
     // Replace RGB for hex
     .replace("RGB", "")
     .replace(".", ",")
-    .replace(rgbCode, (rgbToHex(139,69,19)))
+    .replace(rgbCode, (rgbToHex(139,69,19))) // geef deze cijfers mee aan rgbToHex.. 139 = r, 69 = g, 19 = b
     .replace("", "#")
 );
 
-const showBrown = replaceAll.filter(answer => answer === "#" + hexBrown);
+let showBrown = replaceAll.filter(answer => answer === "#" + hexBrown);
+let showBlue = replaceAll.filter(answer => answer === "#" + hexBlue);
+
+let bestColor = () => (showBlue.length >= showBrown.length) ? "'Blue' (#0099FF) is more common" : "'Brown' (#663300) is more common";
 
 console.log(replaceAll)
-console.log(showBrown)
+console.log("'Brown' (#663300) colors are:", showBrown)
+console.log("'Blue' (#0099FF) colors are:", showBlue)
+console.log(bestColor())
 
 // Write everything in a table (kinda working)
-// const theloop = document.getElementById("loopie")
-// const thecolor = document.getElementsByTagName("td")
-// const holder = theloop;
+const theloop = document.getElementById("loopie")
+const holder = theloop;
 
-// const makeTable = () => {
-//     for(var i = 0; i < replaceAll.length; i++){ 
-//         holder.innerHTML += "<td>" + replaceAll[i] + "</td>";
-//     }
-// }
+const makeTable = () => {
+    for(var i = 0; i < replaceAll.length; i++){ 
+        holder.innerHTML += "<p style='background-color:" + replaceAll[i] + "'>" + replaceAll[i] + "</p>";
+    }
+}
 
-// makeTable();
+makeTable();
 
 // Change all background-colors (not working)
 // const colorChange = () => {
-//     for(var i = 0; i < replaceAll.length; i++){ 
-//         document.getElementsByTagName("td").style.color = replaceAll[i];
+//     for(var v = 0; v < replaceAll.length; v++){ 
+//         document.getElementsByTagName("td").style.color = replaceAll[v];
 //     }
 // }
 
